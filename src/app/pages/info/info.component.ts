0d3666185121b31d2b64bb6ca2a49531bc1b46ea
Teboho-Nyombolo <thulaninyombolo@gmail.com>
2025-05-23 08:16:40 +0200

    Country info done

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CountryService } from '../../services/country.service';
 import { LoaderService } from '../../services/loader.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-info',
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css',
 })
